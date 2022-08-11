1c18cd09bb1af44f40f0874a8becbc014e50a6c4
David Martinez <sdavidmtz022@gmail.com>
2022-08-11 12:48:23 -0500

    Add project files.

---
+++ SDETSelenium/ChromeTest.cs
@@ -0,0 +1,40 @@
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using SDETSelenium.Init;
+
+namespace SDETSelenium
+{
+    public class ChromeTests : BrowserOps
+    {
+        BrowserOps browser = new BrowserOps();
+        string test_url = "https://www.facebook.com";
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            browser.Init_Browser();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            browser.GoTo(test_url);
+            driver = browser.getDriver;
+
+            System.Threading.Thread.Sleep(10000);
+
+            IWebElement logoFacebook = driver.FindElement(By.XPath("//*[@id=\"content\"]/div/div/div/div[1]/div/img"));
+
+            Assert.IsTrue(logoFacebook.Displayed);
+        }
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ SDETSelenium/Init/BrowserOps.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDETSelenium.Init
+{
+    public class BrowserOps
+    {
+        IWebDriver driver;
+        public void Init_Browser()
+        {
+            driver = new ChromeDriver("C:\\Users\\Fecker\\source\\repos\\SDETSelenium\\SDETSelenium\\Drivers");
+            driver.Manage().Window.Maximize();
+        }
+
+        public string Title
+        {
+            get { return driver.Title; }
+        }
+
+        public void GoTo(string url)
+        {
+            driver.Url = url;
+        }
+
+        public void Close()
+        {
+            driver.Quit();
+        }
+
+        public IWebDriver getDriver
+        {
+            get { return driver; }
+        }
+
+    }
+}
